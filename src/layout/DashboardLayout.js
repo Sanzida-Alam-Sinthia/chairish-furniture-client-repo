378b2386db4a65d3aef7f5e02e7619c6a45ff247
Sanzida-Alam-Sinthia <sanzidaalamsinthia@gmail.com>
2022-11-28 14:49:47 +0600

    buyers added

--- src/layout/DashboardLayout.js
+++ src/layout/DashboardLayout.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthProvider/AuthProvider';
 import useAdmin from '../hooks/useAdmin';
+import useBuyer from '../hooks/useBuyer';
 import useSeller from '../hooks/useSeller';
 import Navbar from '../Pages/Navbar/Navbar';
 
@@ -9,6 +10,8 @@ const DashboardLayout = () => {
     const { user } = useContext(AuthContext);
     const [isAdmin] = useAdmin(user?.email)
     const [isSeller] = useSeller(user?.email)
+    const [isBuyer] = useBuyer(user?.email)
+
     return (
         <div>
             <Navbar></Navbar>
@@ -20,24 +23,24 @@ const DashboardLayout = () => {
                 <div className="drawer-side bg-base-200 text-base-content ">
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80  ">
-                        {/* <li><Link to="/dashboard">My Appointments</Link></li> */}
-
-                        {/* <li><Link to="/dashboard/addproduct">Add Product</Link></li> */}
-
-
-
                         {
                             isAdmin && <>
-                                <li><Link to="/dashboard/allusers" className='font-bold text-center'>All users</Link></li>
-                                <li><Link to="/dashboard/allsellers" className='font-bold text-center'>All Sellers</Link></li>
-                                <li><Link to="/dashboard/allbuyers" className='font-bold text-center'>All Buyers</Link></li>
+                                <li><Link to="/dashboard/allusers" className='font-bold text-center text-2xl'>All users</Link></li>
+                                <li><Link to="/dashboard/allsellers" className='font-bold text-center text-2xl'>All Sellers</Link></li>
+                                <li><Link to="/dashboard/allbuyers" className='font-bold text-center text-2xl'>All Buyers</Link></li>
 
                             </>
                         }
                         {
                             isSeller && <>
-                                <li><Link to="/dashboard/addproduct">Add Product</Link></li>
+                                <li><Link to="/dashboard/addproduct" className='font-bold text-center text-2xl'>Add Product</Link></li>
+                                <li><Link to="/dashboard/myproducts" className='font-bold text-center text-2xl'>My Products</Link></li>
                             </>
+                        }
+
+                        {
+                            isBuyer &&
+                            <li><Link to="/dashboard/myorders" className='font-bold text-center text-2xl'>My Orders</Link></li>
                         }
 
                     </ul>
