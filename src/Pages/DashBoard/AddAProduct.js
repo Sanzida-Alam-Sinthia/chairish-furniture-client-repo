00fae9394fab7d13357c0a534d6118fbf727f548
Sanzida-Alam-Sinthia <sanzidaalamsinthia@gmail.com>
2022-11-28 13:18:48 +0600

    add products section added

--- src/Pages/DashBoard/AddAProduct.js
+++ src/Pages/DashBoard/AddAProduct.js
@@ -1,51 +1,46 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const AddAProduct = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-
-    const imageHostKey = process.env.REACT_APP_imgbb_key;
-
+    const { user } = useContext(AuthContext);
+    // console.log(user)
     const navigate = useNavigate();
     const handleAddProduct = data => {
-        const image = data.image[0];
-        const formData = new FormData();
-        formData.append('image', image);
-        const url = `https://api.imgbb.com/1/upload?key=${imageHostKey}`
-        fetch(url, {
+        const product = {
+            category_id: data.catagory,
+            name: data.name,
+            original_price: "0",
+            resell_price: data.price,
+            years_used: data.yearsOfUse,
+            location: data.location,
+            sellers_name: user?.displayName,
+            verified: false,
+            posted_date: "2022-08-24 17:27:34",
+            img: data.img,
+            // condition: data.condition,
+            // description: data.description,
+        }
+        console.log(product)
+
+
+        fetch('http://localhost:5000/products', {
             method: 'POST',
-            body: formData
+            headers: {
+                'content-type': 'application/json',
+                // authorization: `bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify(product)
         })
             .then(res => res.json())
-            .then(imgData => {
-                if (imgData.success) {
-                    console.log(imgData.data.url);
-                    const doctor = {
-                        name: data.name,
-                        email: data.email,
-                        specialty: data.specialty,
-                        image: imgData.data.url
-                    }
+            .then(result => {
+                console.log(result);
+                toast.success(`${data.name} is added successfully`);
 
-                    // save doctor information to the database
-                    fetch('http://localhost:5000/doctors', {
-                        method: 'POST',
-                        headers: {
-                            'content-type': 'application/json',
-                            authorization: `bearer ${localStorage.getItem('accessToken')}`
-                        },
-                        body: JSON.stringify(doctor)
-                    })
-                        .then(res => res.json())
-                        .then(result => {
-                            console.log(result);
-                            toast.success(`${data.name} is added successfully`);
-
-                        })
-                }
             })
     }
     return (
@@ -62,47 +57,57 @@ const AddAProduct = () => {
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"> <span className="label-text">Product Price</span></label>
-                        <input type="text" {...register("number", {
-                            required: "Name is Required"
+                        <input type="number" {...register("price", {
+                            required: "Price is Required"
                         })} className="input input-bordered w-full max-w-xs" />
                         {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
                     </div>
                     <div className="form-control w-full max-w-xs">
-                        <label className="label"> <span className="label-text">Product Name</span></label>
-                        <input type="text" {...register("name", {
-                            required: "Name is Required"
-                        })} className="input input-bordered w-full max-w-xs" />
-                        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                        <label className="label"> <span className="label-text">PickUp Location</span></label>
+                        <input type="text" {...register("location")} className="input input-bordered w-full max-w-xs" />
+
                     </div>
                     <div className="form-control w-full max-w-xs">
-                        <label className="label"> <span className="label-text">Email</span></label>
-                        <input type="email" {...register("email", {
-                            required: true
-                        })} className="input input-bordered w-full max-w-xs" />
-                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                        <label className="label"> <span className="label-text">Product Description</span></label>
+                        <input type="text" {...register("description")} className="input input-bordered w-full max-w-xs" />
+
                     </div>
                     <div className="form-control w-full max-w-xs">
-                        <label className="label"> <span className="label-text">Product Type</span></label>
+                        <label className="label"> <span className="label-text">Year of Use</span></label>
+                        <input type="text" {...register("yearsOfUse")} className="input input-bordered w-full max-w-xs" />
+
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Product Image</span></label>
+                        {/* name="photoURL" type="text" placeholder="Photo URL" */}
+                        <input type="text" {...register("img")} className="input input-bordered w-full max-w-xs" />
+
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Product Condition</span></label>
                         <select
-                            {...register('type')}
+                            {...register('condition')}
                             className="select input-bordered w-full max-w-xs">
 
                             <option defaultChecked>Good</option>
                             <option>Excellent</option>
                             <option>Fair</option>
-
-
-
                         </select>
                     </div>
                     <div className="form-control w-full max-w-xs">
-                        <label className="label"> <span className="label-text">Photo</span></label>
-                        <input type="file" {...register("image", {
-                            required: "Photo is Required"
-                        })} className="input input-bordered w-full max-w-xs" />
-                        {errors.img && <p className='text-red-500'>{errors.img.message}</p>}
+                        <label className="label"> <span className="label-text">Select Catagory</span></label>
+                        <select
+                            {...register('catagory')}
+                            className="select input-bordered w-full max-w-xs">
+
+                            <option defaultChecked>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                        </select>
                     </div>
-                    <input className='btn btn-accent w-full mt-4' value="Add Doctor" type="submit" />
+
+                    <input className='btn btn-accent w-full mt-4' value="Add Product" type="submit" />
                 </form>
             </div>
         </div>
